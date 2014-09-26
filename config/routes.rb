c2a229d56f606ec35f45309ebddb27dd39cd82fd
Murtuza Kutub <murtuzafirst@gmail.com>
2014-09-27 04:29:37 +0530

    add indes routes for users and tips

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  resources :users, :only => [:create]
-  resources :tips, :only => [:create]
+  resources :users, :only => [:create, :index]
+  resources :tips, :only => [:create, :index]
 end
