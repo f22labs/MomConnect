124586b7bff39e2cef8d89771812b87528c9e78a
HariHaraSudhan <hariharasudhan@live.com>
2014-09-27 05:39:17 +0530

    MAJOR BUG FIX

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
 
   def create
-    user_params[:user][:date] = Time.at(user_params[:user][:date]).to_i
+    user_params[:conception_timestamp] = Time.new(user_params[:conception_timestamp].split("/")[2], user_params[:conception_timestamp].split("/")[0], user_params[:conception_timestamp].split("/")[1]).to_i
     render :json => User.create!(user_params)
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:phone, :date, :locale_id)
+    params.require(:user).permit(:phone, :conception_timestamp, :locale_id)
   end
 end
